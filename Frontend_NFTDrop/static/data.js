85bace3c9f99d2a6afe2147eb6777e41cc285822
BhattacharyaKun <bhattacharyakun@gmail.com>
2023-06-30 22:22:14 +0530

    Candy machien v3

---
+++ Frontend_NFTDrop/static/data.js
@@ -0,0 +1,34 @@
+export const data = {
+    "program": {
+        "candyMachine": "9rY7xSHnmfvenkwGmweFFnEmADQXTULGDwgcczYMbjR7",
+        "candyGuard": "",
+        "candyMachineCreator": "CdmBLjRGi7RJSLDWRAmfcXEswwrXbTUry274Ko2n7F43",
+        "collectionMint": "HZUu9VLcbUW8z3RwHgzcFYxiSpNgor6ahZLGM9MRy6Hi"
+    },
+    "items": [
+        {
+            "name": "House #1",
+            "image_hash": "cda6b7a466eb695e4385709b17bba222586490919a6b5768acc1d457960f2ca2",
+            "image_link": "https://nftstorage.link/ipfs/bafybeie7d4w6nbv6ri5345ofl7fhjnz3l45cunqql3beqcxas435ewai6a/0.png",
+            "metadata_hash": "92cc41c11c82c67caefb415f97d3390647bf8c533038f4ab536e1f15f855b393",
+            "metadata_link": "https://nftstorage.link/ipfs/bafybeie6ishdpchnqd5qfxols6ph27k4ho2t37kcflchfgdiwhlx24iepa/0.json",
+            "onChain": true
+          },
+          {
+            "name": "House #2",
+            "image_hash": "3ba9b3e1e70759e33135dc17b829b2e412d07d978f1e96e29190a97423dda779",
+            "image_link": "https://nftstorage.link/ipfs/bafybeie7d4w6nbv6ri5345ofl7fhjnz3l45cunqql3beqcxas435ewai6a/1.png",
+            "metadata_hash": "2d0eea01816462f3fe8e9851568dcce760f2e90a1b50c72afd1a646086ec7b10",
+            "metadata_link": "https://nftstorage.link/ipfs/bafybeie6ishdpchnqd5qfxols6ph27k4ho2t37kcflchfgdiwhlx24iepa/1.json",
+            "onChain": true
+          },
+          {
+            "name": "House #3",
+            "image_hash": "5d570d585d7f27158cd27a3ff4af944ae81a33ab3d6c0e151a70fa1b1f0e5f92",
+            "image_link": "https://nftstorage.link/ipfs/bafybeie7d4w6nbv6ri5345ofl7fhjnz3l45cunqql3beqcxas435ewai6a/2.png",
+            "metadata_hash": "48f3c9f15d8909cebc42c72ddaf9e4be57d0fd3a143a3bebb0a3431625995765",
+            "metadata_link": "https://nftstorage.link/ipfs/bafybeie6ishdpchnqd5qfxols6ph27k4ho2t37kcflchfgdiwhlx24iepa/2.json",
+            "onChain": true
+          },
+    ]
+}
---
+++ Frontend_NFTDrop/utils/constants.js
@@ -0,0 +1,5 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const RPC_ENDPOINT = "https://api.devnet.solana.com";
+
+export const CANDY_MACHINE_ID = new PublicKey("5wJDN6XPGc9Nc2my7FnY16FhzajuykWzhtbN1fBxVimp");
